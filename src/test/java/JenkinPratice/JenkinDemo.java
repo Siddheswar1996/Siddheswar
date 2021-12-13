effb647442b9b22b9d47632d423fbbba95aa7f79
admin <admin@DESKTOP-4UA967F>
2021-12-13 01:17:08 -0800

    jenkin first

---
+++ src/test/java/JenkinPratice/JenkinDemo.java
@@ -0,0 +1,11 @@
+package JenkinPratice;
+
+import org.testng.annotations.Test;
+
+public class JenkinDemo {
+	@Test
+	public void jenkinDemo() {
+		System.out.println("my first jenkin demo");
+	}
+
+}
